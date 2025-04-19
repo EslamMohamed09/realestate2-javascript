7c563841fa12ba125aa8ed334a703ba1afbbe8be
EslamMayo09 <eslammayo2022@gmail.com>
2025-04-20 00:09:19 +0200

    Achieve apartments & villas and builds & lands section after modify responsiviness in dotsFullResponsiveSlider

--- inc/js/main.js
+++ inc/js/main.js
@@ -263,26 +263,25 @@ document.querySelector(".hero-section").style.height = (window.innerHeight - hea
  #### SELL APARTMENTS SECTION ####
  #################################
 */
-// dotsFullResponsiveSlider({
-//     section:'.sell-apartments-section',
-//     containerSelector:'.sell-apartments-section .slider-wrapper',
-//     dotsSelector:'.sell-apartments-section #sliderdots',
-//     prevArrowSelector:'.sell-apartments-section .arrow-left',
-//     nextArrowSelector:'.sell-apartments-section .arrow-right',
-// });
+dotsFullResponsiveSlider({
+    sectionSelector:'.sell-apartments-section',
+    containerSelector:'.sell-apartments-section .slider-wrapper',
+    prevArrowSelector:'.sell-apartments-section .arrow-left',
+    nextArrowSelector:'.sell-apartments-section .arrow-right',
+});
 
-/* 
+/*
  #############################
  #### SELL BUILDS SECTION ####
  #############################
 */
-// dotsFullResponsiveSlider({
-//     section:'.sell-builds-section',
-//     containerSelector:'.sell-builds-section .slider-wrapper',
-//     dotsSelector:'.sell-builds-section #sliderdots',
-//     prevArrowSelector:'.sell-builds-section .arrow-left',
-//     nextArrowSelector:'.sell-builds-section .arrow-right',
-// });
+dotsFullResponsiveSlider({
+    sectionSelector:'.sell-builds-section',
+    containerSelector:'.sell-builds-section .slider-wrapper',
+    prevArrowSelector:'.sell-builds-section .arrow-left',
+    nextArrowSelector:'.sell-builds-section .arrow-right',
+});
+
 
 /* 
  #################################
